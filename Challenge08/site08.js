7560a4afef597168e0381e5c7b07f0145e530a68
Ma77hewHill <61993268+Ma77hewHill@users.noreply.github.com>
2023-02-09 17:39:26 +0000

    Challenge 9

--- Challenge08/site08.js
+++ Challenge08/site08.js
@@ -1,5 +1,6 @@
 const numbers = [9, 1, 3, 27, 15, -5, 13, 7];
 
+
 //driver function used for display and passing values.
 function displayNumbers() {
     
@@ -8,7 +9,7 @@ function displayNumbers() {
     let avg = avgNumber(numbers);
     let max = maxNumber(numbers);
     let min = minNumber(numbers);
-    let sumRec = sumNumbersRecursive(numbers,index);
+    
 
     //used for display not need to change unless you are doing only the sum function
     let msg = `Sum = ${sum}<br>Avg = ${avg.toFixed(2)}<br>Max = ${max}<br>Min = ${min}`;
@@ -20,7 +21,13 @@ function displayNumbers() {
 
 //takes an array of numbers and returns the sum of all the numbers
 function sumNumbers(numArry) {
-    return 0;
+    let sum = 0;
+
+    for (let index = 0; index < numArry.length; index++) {
+        sum += numArry[index];
+        
+    }
+    return sum;
 }
 
 //takes an array of numbers and the current index in the array and returns the sum of all the numbers using recursion
@@ -33,15 +40,40 @@ function sumNumbersRecursive(numArry, index) {
 
 //takes an array of numbers and returns the average of all the numbers
 function avgNumber(numArry) {
+    let sum = 0;
+    let average = 0;
+    for (let index = 0; index < numArry.length; index++) {
+        sum += numArry[index];
+        average = sum / numArry.length;
+    }
+    return average;
     return 0;
 }
 
 //takes an array of numbers and returns the max number
 function maxNumber(numArry) {
-    return 0;
+    let max = numArry[0];
+
+    for (let index = 1; index < numArry.length; index++) {
+        if (numArry[index] > max) {
+            max = numArry[index];
+        }
+        
+    }
+    return max;
 }
 
 //takes an array of numbers and returns the min number
 function minNumber(numArry) {
+
+    let min = numArry[0];
+
+    for (let index = 1; index < numArry.length; index++) {
+        if (numArry[index] < min) {
+            min = numArry[index];
+        }
+        
+    }
+    return min;
     return 0;
 }
